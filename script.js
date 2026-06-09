d378b4a9f1d563772b85bc0a9fceb55738ecc47d
ckhuc <christopherkhuc@gmail.com>
2026-06-09 16:38:41 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function pickDate(choice) {
+  const result = document.getElementById("result");
+
+  if (choice === "rock climbing") {
+    result.textContent = "Great choice. I was hoping you’d pick rock climbing 🧗";
+  } else if (choice === "escape room") {
+    result.textContent = "Escape room it is. Let’s see if we’re a good team 🔐";
+  } else if (choice === "movie") {
+    result.textContent = "Movie date sounds perfect. Popcorn is mandatory 🎬";
+  }
+}
